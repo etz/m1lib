815791f7b206e44209e25286ed329083ba7b249e
Ethan Nadzieja <ethan.nadzieja@gmail.com>
2019-03-27 03:49:31 -0700

    Additional Functions

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ driver= webdriver.Chrome(options=chromeOptions,desired_capabilities=capabilities
 
 username = ""
 password = ""
+accountType = "IRA"
 
 #Go to M1 Finance's Website
 #Log-in with the account information
@@ -28,7 +29,24 @@ def login(m1user, m1pass):
 
 login(username, password)
 
-#Access the Roth IRA
+#Function: CheckOrderStatus
+#Usage: Returns Order value depending on accType
+def CheckOrderStatus(accType):
+    selectAccount(accType)
+    orderCompletion = driver.find_element_by_xpath("""//*[@id="root"]/div/div/div[1]/div[2]/div/div[2]/div/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div/div[1]/span""").text
+    if "Pending buy" in orderCompletion:
+        print("PENDING:" + orderCompletion)
+
+#Function: CheckDayGain
+#Usage: Provides the USD return for the accType
+def CheckDayGain(accType):
+    selectAccount(accType)
+    driver.find_element_by_xpath("""//*[@id="root"]/div/div/div[1]/div[2]/div/div[2]/div/div[1]/div[2]/div[1]/div[2]/div/div/span[1]""").click()
+    time.sleep(3)
+    print (driver.find_element_by_xpath("""//*[@id="root"]/div/div/div[1]/div[2]/div/div[2]/div/div[1]/div[2]/div[2]/div[1]/div/div[2]/div[1]/div[2]/span""").text)
+
+#Function: selectAccount
+#Usage: Determines if the account being edited is the one we intend
 def selectAccount(accType):
     accountType = driver.find_element_by_xpath("""//*[@id="popup22"]/div[1]/h3""").text
     if accType not in accountType:
@@ -37,13 +55,39 @@ def selectAccount(accType):
         driver.find_element_by_xpath("""//*[@id="popup22"]/div[2]/div/div[2]""").click()
         time.sleep(5)
         selectAccount(accType)
-    if accType in accountType:
-        driver.find_element_by_xpath("""//*[@id="root"]/div/div/div[1]/div[2]/div/div[2]/div/div[1]/div[2]/div[1]/div[2]/div/div/span[1]""").click()
-        time.sleep(3)
-        print (driver.find_element_by_xpath("""//*[@id="root"]/div/div/div[1]/div[2]/div/div[2]/div/div[1]/div[2]/div[2]/div[1]/div/div[2]/div[1]/div[2]/span""").text)
+    time.sleep(2)
 
-selectAccount("IRA")
+#Function: BuyPie
+#Usage: Purchases a M1 Pie based on the USD value and the accType
+def BuyPie(amount, accType):
+    selectAccount(accType)
+    if (amount < 10):
+        print("Orders under $10 cannot be processed")
+        return
+    driver.find_element_by_xpath("""//*[@id="root"]/div/div/div[1]/div[2]/div/div[2]/div/div[1]/div[1]/div/div[1]/div[3]/div/button[1]""").click()
+    time.sleep(4)
+    try:
+        usernameField = driver.find_element_by_name("cashFlow")
+        usernameField.send_keys(amount)
+    except:
+        print("It appears there is already a concurrent order.")
+        return
+    driver.find_element_by_xpath("""//*[@id="root"]/div/div/div[1]/div[2]/span/div/div[2]/div/div/div/div/div[6]/button[2]""").click()
+    time.sleep(3)
+    driver.find_element_by_xpath("""//*[@id="root"]/div/div/div[1]/div[2]/span/div/div[2]/div/div/div/div/div[9]/button[2]""").click()
+    time.sleep(6)
+    try:
+        orderCompletion = driver.find_element_by_xpath("""//*[@id="root"]/div/div/div[1]/div[2]/div/div[2]/div/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div/div[1]/span""").text
+    except:
+        print ("There was an issue completing your order as intended.")
+        BuyPie(amount, accType)
+    if "buy" in orderCompletion:
+        print ("Purchase successful! " + amount + "$ bought")
 
-print("Done")
+CheckDayGain(accountType)
+BuyPie(15, accountType)
+CheckOrderStatus(accountType)
+
+print("Script Complete")
 time.sleep(20)
 driver.close()
