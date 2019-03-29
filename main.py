3bb115c35593ebb344ffb775af5b5373a0594dad
Ethan Nadzieja <ethan.nadzieja@gmail.com>
2019-03-29 03:30:19 -0700

    Testing Search

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import config
 
 if (m1lib.login(config.username, config.password) == True):
     m1lib.DebugCommand("I successfully ran the login function.")
-
+    m1lib.tickerSearch(aum_min='0',aum_max='',pe_min='1',pe_max='',div_min='3',div_max='5',sector='Basic Materials',industry='Coal')
+    #m1lib.selectAccount("Roth")
     #and your magic here... 😄
 
     #Some examples:
@@ -14,16 +15,16 @@ if (m1lib.login(config.username, config.password) == True):
     #m1lib.orderPV(50, config.accountType) #Purchases $50 from config.accountType
     #m1lib.orderPV(-50, "Individual") #Sells $50 from "Individual" account
 
-    dayReturn = m1lib.checkReturnsPV(config.accountType, "week") #day/week/month/year/all
-    print (dayReturn) #Returns float value
-    dayReturn = m1lib.checkReturnsPie(m1lib.getPID(config.accountType), "week")
-    print (dayReturn) #Returns same float value
+    #dayReturn = m1lib.checkReturnsPV(config.accountType, "week") #day/week/month/year/all
+    #print (dayReturn) #Returns float value
+    #dayReturn = m1lib.checkReturnsPie(m1lib.getPID(config.accountType), "week")
+    #print (dayReturn) #Returns same float value
 
 
-    AAPL = m1lib.searchTicker("aapl")
+    #AAPL = m1lib.searchTicker("aapl")
     #m1lib.initiateDeposit(50, "Roth", 2019)
-    m1lib.changeAutoInvest("Roth", option="off", amount='')
-    m1lib.changeAutoInvest("Individual", "all")
+    #m1lib.changeAutoInvest("Roth", option="off", amount='')
+    #m1lib.changeAutoInvest("Individual", "all")
     #m1lib.changeAutoInvest("Roth", option="set", amount='10')
 #End your file with...
-m1lib.closeSession()
+#m1lib.closeSession()
